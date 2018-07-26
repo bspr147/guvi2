44d302158f0bfbeff91f8366283e8e6bd11cb2a6
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-07-26 21:32:05 +0530

    Update positivie.java

--- positivie.java
+++ positivie.java
@@ -5,24 +5,17 @@ import java.lang.*;
 import java.io.*;
 class Ideone
 {
-	public static void main (String[] args)
+	int n=3;
+	if(n>0)
 	{
-		int n=3;
-		if(n>0)
-		{
-			System.out.print("Positive");
-		}
-		else if(n<0)
-		{
-			System.out.print("negative");
-		}
-		else if(n==0)
-		{
-			Sysytem.out.print("Zero");
-		}
-		else
-		{
-			System.out.print("nothing");
-		}
+		System.out.print("Positive");
+	}
+	else if(n>0)
+	{
+		System.out.print("negative");
+	}
+	else if(n==0)
+	{
+		System.out.print("Zero");
 	}
 }
