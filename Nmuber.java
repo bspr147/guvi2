417d675db521fafad60dea389d6c9a4d5d6b4199
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-07-27 12:20:27 +0530

    Create Nmuber.java

---
+++ Nmuber.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Number
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	// char n=sc.next().charAt(0);
+	 int m=sc.nextInt();
+	 int i;
+	 for(i=1;i<=m;i++)
+	 {
+	 	System.out.println("Hello");
+	 }
+	 
+	}
+}
