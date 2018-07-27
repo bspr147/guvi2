4142ec52b00183374bb98dc7aaf26fb4fd2988c0
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-07-27 13:02:49 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Count
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner sc=new Scanner(System.in);
+ int m=sc.nextInt();
+ int count=0;
+  
+ // int sum=0;
+  while(m!=0)
+  
+  {
+          m=m/10;
+          count++;
+ 
+  }
+  System.out.println(count);
+	}
+}
