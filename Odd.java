bf9a7a2ca6f86591b148355b11c502c0e72bafce
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-07-30 16:23:19 +0530

    Update Odd.java

--- Odd.java
+++ Odd.java
@@ -4,7 +4,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Odd
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -16,7 +15,7 @@ class Odd
 	{
 		if(i%2==1)
 		{
-		System.out.print(i+" ");	
+		System.out.print(" "+i+" ");	
 		}
 	}
 
