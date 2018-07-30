5154ac422071ca08e5fd98c349f47466ba2ced45
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-07-30 16:14:58 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Odd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int m=s.nextInt();
+	for(int i=n;i<=m;i++)
+	{
+		if(i%2==1)
+		{
+		System.out.print(i+" ");	
+		}
+	}
+
+	}
+}
