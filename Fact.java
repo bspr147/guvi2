5284c016c1facf7d3a52f898901669d38d140094
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-07-31 11:24:42 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Amst
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	int sum=1;
+	for(int i=1;i<=a;i++)
+	{
+		sum=sum*i;
+		
+	}
+	System.out.print(sum);
+
+	}
+}
+
+	
+
+	
+
