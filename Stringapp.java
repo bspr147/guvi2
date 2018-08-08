c8ebd19be962926250a40450340f634905815e6f
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-08-08 21:34:58 +0530

    Create Stringapp.java

---
+++ Stringapp.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Stringapp
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String S=s.next();
+		String stl=".";
+		String c=S.concat(stl);
+		System.out.print(c);
+		
+		
+	}
+}
