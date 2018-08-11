c7a30639d7ba9e0721b2aa06a968adbee8a2eec2
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-08-11 12:03:28 +0530

    Update Smallof.java

--- Smallof.java
+++ Smallof.java
@@ -1,28 +1,31 @@
+/* package whatever; // don't place package name! */
 
-		
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
- class Smallof
+/* Name of the class has to be "Main" only if the class is public. */
+class Min
 {
-   public static void main(String args[])
-   {
-       int large,N,i;
-       int arr[] = new int[100000];
-       Scanner scan = new Scanner(System.in);
-       N= scan.nextInt();
-       for(i=0;i<N;i++)
-       {
-           arr[i] = scan.nextInt();
-       }
-       large = arr[0];
-	   
-       for(i=0;i<N;i++)
-       {
-           if(large > arr[i])
-           {
-               large = arr[i];
-           }
-          }
-          System.out.print(large+" "); 
-   }
+public static void main (String[] args) throws java.lang.Exception
+{
+// your code goes here
+int min;
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+min=a[0];
+for(int i=0;i<n;i++)
+{
+if(min>a[i])
+{
+min=a[i];
+}
+}
+System.out.println(min);
+}
 }
