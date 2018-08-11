12c884e81723abc8b77bffe5eb9fe4684b7cdcd0
bspr147 <41512482+bspr147@users.noreply.github.com>
2018-08-11 11:10:03 +0530

    Create Smallof.java

---
+++ Smallof.java
@@ -0,0 +1,28 @@
+
+		
+import java.util.Scanner;
+
+ class Smallof
+{
+   public static void main(String args[])
+   {
+       int large,N,i;
+       int arr[] = new int[100000];
+       Scanner scan = new Scanner(System.in);
+       N= scan.nextInt();
+       for(i=0;i<N;i++)
+       {
+           arr[i] = scan.nextInt();
+       }
+       large = arr[0];
+	   
+       for(i=0;i<N;i++)
+       {
+           if(large > arr[i])
+           {
+               large = arr[i];
+           }
+          }
+          System.out.print(large+" "); 
+   }
+}
